ac5f50a32503974013d3a0f0986f028b80e4fd35
sunqi <sq08201329@126.com>
2016-09-20 22:09:01 +0800

    schtasks

--- bash.sh
+++ bash.sh
@@ -1,3 +1,4 @@
-a
-b
-c
+#C:\Windows\System32\schtasks.exe /create /sc minute /mo 1 /tn "sunqi" /tr D:\Python\me\gitStroe\bash.sh
+#C:\Windows\System32\schtasks.exe  | findstr /i "sunqi"
+dt=$(date "+%Y年%m月%d日")
+echo $dt >> ~/Desktop/cmd.txt
