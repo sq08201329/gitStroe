01cc39d36a73303ed241ee70451e6c7acf75beef
sunqi <sq08201329@126.com>
2016-09-27 22:16:56 +0800

    auto commit at 2016年09月27日 22:16:56 docx successful

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,6 +1,6 @@
 git pull
 git add *
 git status
-git commit -m "auto commit at $(date)"
+git commit -m "auto commit at $(date) $1"
 git diff HEAD~1  HEAD
 git push origin master
---
+++ dx.py
@@ -0,0 +1,14 @@
+import docx
+
+docName = 'D:\Python\me\gitStroe\doc-test.docx'
+
+def readDocx(docName):
+    fullText = []
+    doc = docx.Document(docName)
+    paras = doc.paragraphs
+    for p in paras:
+        fullText.append(p.text)
+    return '\n'.join(fullText)
+
+print(readDocx(docName))
+
