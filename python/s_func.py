4d1abd9981dead5145176d620bcbde87d0273277
sunqi <sq08201329@126.com>
2016-10-03 12:45:42 +0800

    auto commit at 2016年10月 3日 12:45:42

--- python/s_func.py
+++ python/s_func.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env.exe python
 #from mo import func
 tp = (int, float)
-inp = input()
-if not isinstance(inp, tp):		#check type
-	raise TypeError('sunqi')
+tp2 = (float, str)
+inp = input('input a num:')
+if not isinstance(inp, tp2):		#check type
+	#raise TypeError('sunqi')
+	print('type error')
+	pass
+else:
+	print('is a num')
+
+#help(isinstance)
+
+def add_end(L=None):
+	if L is None:
+		L = []
+	L.append('END')
+	return L
+print(add_end([1,2,3]))
